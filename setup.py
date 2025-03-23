e53d670b9352fb3906581180f885d9730cd54a5a
zoomer_k <ikoeg2000-efegrrg@yahoo.co.jp>
2025-03-24 00:15:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from wheel.bdist_wheel import bdist_wheel
 class Build(build):
     def run(self):
         if isdir("queries"):
-            dest = join(self.build_lib, "tree_sitter_verilog", "queries")
+            dest = join(self.build_lib, "tree_sitter_systemverilog", "queries")
             self.copy_tree("queries", dest)
         super().run()
 
@@ -26,15 +26,15 @@ setup(
     packages=find_packages("bindings/python"),
     package_dir={"": "bindings/python"},
     package_data={
-        "tree_sitter_verilog": ["*.pyi", "py.typed"],
-        "tree_sitter_verilog.queries": ["*.scm"],
+        "tree_sitter_systemverilog": ["*.pyi", "py.typed"],
+        "tree_sitter_systemverilog.queries": ["*.scm"],
     },
-    ext_package="tree_sitter_verilog",
+    ext_package="tree_sitter_systemverilog",
     ext_modules=[
         Extension(
             name="_binding",
             sources=[
-                "bindings/python/tree_sitter_verilog/binding.c",
+                "bindings/python/tree_sitter_systemverilog/binding.c",
                 "src/parser.c",
             ],
             extra_compile_args=[
